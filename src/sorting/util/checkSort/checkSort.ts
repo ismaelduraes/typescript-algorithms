cd721133abec98826689d6ee7bf484e060087e22
halaxysounds <halaxysounds@gmail.com>
2022-05-04 18:23:46 -0300

    chore: refactor utils folder

--- src/sorting/util/checkSort/checkSort.ts
+++
@@ -1,9 +0,0 @@
-export default function checkSort(list: number[]): boolean {
-  let isSorted = true;
-
-  for (let i = 0; i <= list.length; i++) {
-    if (list[i] > list[i + 1]) isSorted = false;
-  }
-
-  return isSorted;
-}
--- src/sorting/util/cloneList/cloneList.ts
+++
@@ -1,9 +0,0 @@
-export default function cloneList(list: number[]): number[] {
-  const clonedList = [];
-
-  for (let i = 0; i < list.length; i++) {
-    clonedList.push(list[i]);
-  }
-
-  return clonedList;
-}
